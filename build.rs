8bc37f27f96893b09bc63439474a2acbd2043f81
robin <robin@ansvia.com>
2019-02-04 01:37:58 +0700

    [Enhancement] Jangan taruh autogen proto file di dalam src, tapi taruh di `OUT_DIR` lalu di-include via `mod.rs`, hal ini dikarenakan oleh: https://github.com/rust-lang/cargo/pull/5584
    Tambah beberapa proto message.

--- build.rs
+++ build.rs
@@ -1,10 +1,18 @@
 extern crate protoc_rust;
 
 use protoc_rust::{Args, Customize};
+use std::{
+    env,
+    fs,
+    // fs::File,
+    // io::{BufRead, BufReader, BufWriter},
+};
 
 fn main() {
+    let out_dir = env::var("OUT_DIR").expect("Cannot get OUT_DIR");
+
     protoc_rust::run(Args {
-        out_dir: "src/protos",
+        out_dir: &out_dir,
         input: &["src/protos/apf.proto"],
         includes: &["src/protos"],
         customize: Customize {
@@ -14,5 +22,20 @@ fn main() {
     })
     .expect("Protoc compile");
 
-    println!("cargo:rerun-if-changed={}", "src/protos");
+    let path = format!("{}/apf.rs", out_dir);
+    let content = fs::read_to_string(&path).expect("cannot read autogen protobuf apf.rs");
+    let mut new_content = vec![];
+
+    for line in content.split("\n") {
+        if line.starts_with("#![") {
+            continue;
+        }
+        new_content.push(line);
+    }
+
+    let new_content: String = new_content.join("\n");
+
+    let _ = fs::write(&path, new_content);
+
+    println!("cargo:rerun-if-changed={}", "src/protos/apf.proto");
 }
