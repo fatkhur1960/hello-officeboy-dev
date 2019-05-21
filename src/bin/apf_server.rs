f893c310a638b36811fb2cc1fa1285ea5fe84884
fatkhur1960 <36976918+fatkhur1960@users.noreply.github.com>
2019-05-21 23:12:10 +0700

    Update apf_server.rs

--- src/bin/apf_server.rs
+++ src/bin/apf_server.rs
@@ -1,4 +1,4 @@
-extern crate apf;
+// extern crate apf;
 
 #[macro_use]
 extern crate log;
@@ -6,6 +6,7 @@ extern crate dotenv;
 extern crate env_logger;
 
 use apf::prelude::*;
+use apf::service::AuthService;
 
 use std::env;
 
@@ -13,37 +14,40 @@ fn main() {
     dotenv::dotenv().ok();
     env_logger::init();
 
-    println!(
-        r#"
+//     println!(
+//         r#"
 
-____________    __      ____________,
-\_____     /   /_ \     \     _____/
- \_____    \____/__\____/    _____/
-  \_____      | APF |       _____/
-     \________\__|__/_________/
-               /___\
-            ._//___\\_.
-    "#
-    );
+// ____________    __      ____________,
+// \_____     /   /_ \     \     _____/
+//  \_____    \____/__\____/    _____/
+//   \_____      | APF |       _____/
+//      \________\__|__/_________/
+//                /___\
+//             ._//___\\_.
+//     "#
+//     );
 
     println!(
-        "\nAPF server {}\n_______________________________________\n{}\ngit: {}\n",
+        "\nHello Officeboy server {}\n_______________________________________\n{}\ngit: {}\n",
         env!("CARGO_PKG_VERSION"),
         env!("BUILD_INFO"),
         env!("GIT_REV")
     );
 
-    trace!("starting up...");
+    trace!("Starting up the server...");
 
-    let service = PaymentService::new();
+    let auth_service = AuthService::new();
+    let payment_service = PaymentService::new();
 
-    let public_listening_address = env::var("APF_PUBLIC_LISTENING").unwrap_or("0.0.0.0:8080".to_string());
-    let private_listening_address = env::var("APF_PRIVATE_LISTENING").unwrap_or("127.0.0.1:9090".to_string());
+    let public_listening_address =
+        env::var("APF_PUBLIC_LISTENING").unwrap_or_else(|_| "0.0.0.0:8080".to_string());
+    let private_listening_address =
+        env::var("APF_PRIVATE_LISTENING").unwrap_or_else(|_| "0.0.0.0:9090".to_string());
 
     let config = ServiceApiConfig::new(vec![
         ApiServer::new(ApiAccess::Public, public_listening_address),
         ApiServer::new(ApiAccess::Private, private_listening_address),
     ]);
 
-    api::start(ApiAggregator::new(vec![service]), config);
+    api::start(ApiAggregator::new(vec![auth_service, payment_service]), config);
 }
